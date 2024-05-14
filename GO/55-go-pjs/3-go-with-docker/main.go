88c1f78cfbdba3092c04346ef03317c8827c0875
Ned Groom <ned.groom@gmail.com>
2024-05-14 22:07:12 +0900

    Tutorial instructions, but --no such file ./app/main-- so not working on --docker run--

---
+++ GO/55-go-pjs/3-go-with-docker/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html" // to print out the html response onto the browser, otherwise can use browser
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
