c4ec06b962fb2207176646aa452cbd05e258ffb2
Ned Groom <ned.groom@gmail.com>
2024-05-18 15:38:18 +0900

    correct formation of mysql connection, now all working well

--- GO/55-go-pjs/4-go-mysql-bookstore/pkg/config/app.go
+++ GO/55-go-pjs/4-go-mysql-bookstore/pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "nedg:mypassword/tablename?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "nedgroom:chocolate@tcp/neddb?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
